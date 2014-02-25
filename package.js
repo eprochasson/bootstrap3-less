09c1c8fe67018667f50d37a5712fc7ce22666768
Mikael Korpela <mikael@ihminen.org>
2014-02-25 11:27:50 +0200

    Update Bootstrap v3.0.3 => v3.1.1

--- package.js
+++ package.js
@@ -1,15 +1,19 @@
 Package.describe({
-    summary: "Bootstrap 3, with Less files (v3.0.3)."
+  summary: "Bootstrap 3, with Less files (v3.1.1)."
 });
 
 Package.on_use(function (api) {
-    api.use('jquery', 'client');
-    api.use('less','client');
-    api.add_files('lib/js/bootstrap.js', 'client');
+  api.use('jquery', 'client');
+  api.use('less','client');
 
-    // fonts
-    api.add_files('lib/fonts/glyphicons-halflings-regular.eot', 'client');
-    api.add_files('lib/fonts/glyphicons-halflings-regular.ttf', 'client');
-    api.add_files('lib/fonts/glyphicons-halflings-regular.svg', 'client');
-    api.add_files('lib/fonts/glyphicons-halflings-regular.woff', 'client');
+  var path = Npm.require('path');
+  var asset_path = path.join('dist');
+
+  api.add_files(path.join(asset_path, 'js', 'bootstrap.js'), 'client');
+
+  // fonts
+  api.add_files(path.join(asset_path, 'fonts', 'glyphicons-halflings-regular.eot'), 'client');
+  api.add_files(path.join(asset_path, 'fonts', 'glyphicons-halflings-regular.ttf'), 'client');
+  api.add_files(path.join(asset_path, 'fonts', 'glyphicons-halflings-regular.svg'), 'client');
+  api.add_files(path.join(asset_path, 'fonts', 'glyphicons-halflings-regular.woff'), 'client');
 });
